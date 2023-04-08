7ab511549cbe9fae19ee638027d62b3f85faa848
AdrianoGullo <adrianogullo1@gmail.com>
2023-04-08 12:40:28 -0300

    Xadrez+Snake

---
+++ pagport/Snake.js
@@ -0,0 +1,17 @@
+const ImgSnake = document.getElementById("Snake");
+const box1 = document.getElementById("box1");
+const boxCalculadora = document.getElementById("boxCalculadora");
+
+ImgXadrez.addEventListener('click', function() {
+
+    if (boxCalculadora.style.display === 'block') {
+        boxCalculadora.style.display = 'none';
+      } else {
+        boxCalculadora.style.display = 'block';
+      }
+      if (box1.style.display === 'block') {
+        box1.style.display = 'none';
+      } 
+
+      
+});
