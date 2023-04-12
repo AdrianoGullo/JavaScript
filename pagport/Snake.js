0593b7e3c288d88a2ba2fedc73bf266907ef40cb
AdrianoGullo <adrianogullo1@gmail.com>
2023-04-12 01:07:04 -0300

    SnakeUpdate

--- pagport/Snake.js
+++ pagport/Snake.js
@@ -1,8 +1,22 @@
+const GAME_WIDTH = 608;
+const GAME_HEIGHT = 608;
+const GAME_AREA_X = 0;
+const GAME_AREA_Y = 0;
+
 const ImgSnake = document.getElementById("start-game");
 const box1 = document.getElementById("boxSnake");
 const canvas = document.getElementById("snake");
 const ctx = canvas.getContext("2d");
 const box = 32;
+let speed = 150; // velocidade atual da cobra
+let intervalId;
+
+// carrega a imagem da cobra
+const CabeçaCobra = new Image();
+CabeçaCobra.src = 'pagport/Imagens/SnakeEyes/cabeçacobra.jpg';
+
+const CorpoCobra = new Image();
+CorpoCobra.src = 'pagport/Imagens/SnakeEyes/corpocobra.jpg';
 
 document.addEventListener("keydown", function(event) {
   switch(event.key) {
@@ -18,8 +32,8 @@ document.addEventListener("keydown", function(event) {
 });
 
 let food = {
-  x: Math.floor(Math.random() * 17 + 1) * box, 
-  y: Math.floor(Math.random() * 15 + 3) * box,
+  x: Math.floor(Math.random()) * box,
+  y: Math.floor(Math.random()) * box
 };
 
 let snake = [{
@@ -33,8 +47,13 @@ let lastDirection;
 // desenha a cobra na tela
 function drawSnake() {
   for(let i = 0; i < snake.length; i++){
-    ctx.fillStyle = "green";
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    if(i === 0) {
+      // Desenha a cabeça da cobra com a imagem
+      ctx.drawImage(CabeçaCobra, snake[i].x, snake[i].y, box, box);
+    } else {
+      // Desenha o corpo da cobra com a imagem
+      ctx.drawImage(CorpoCobra, snake[i].x, snake[i].y, box, box);
+    }
   }
 }
 
@@ -57,26 +76,26 @@ function update() {
   // atualiza a posição da cabeça da cobra com base na última direção escolhida pelo usuário
   if (lastDirection === "right") {
     snakeX += box;
-    if (snakeX > 17 * box) {
-      snakeX = 0;
+    if (snakeX >= GAME_AREA_X + GAME_WIDTH) {
+      snakeX = GAME_AREA_X;
     }
   }
   if (lastDirection === "left") {
     snakeX -= box;
-    if (snakeX < 0) {
-      snakeX = 17 * box;
+    if (snakeX < GAME_AREA_X) {
+      snakeX = GAME_AREA_X + GAME_WIDTH - box;
     }
   }
   if (lastDirection === "up") {
     snakeY -= box;
-    if (snakeY < 3 * box) {
-      snakeY = 17 * box;
+    if (snakeY < GAME_AREA_Y) {
+      snakeY = GAME_AREA_Y + GAME_HEIGHT - box;
     }
   }
   if (lastDirection === "down") {
     snakeY += box;
-    if (snakeY > 18 * box) {
-      snakeY = 3 * box;
+    if (snakeY >= GAME_AREA_Y + GAME_HEIGHT) {
+      snakeY = GAME_AREA_Y;
     }
   }
 
@@ -86,8 +105,8 @@ function update() {
     snake.unshift(newHead);
     // cria uma nova comida
     food = {
-      x: Math.floor(Math.random() * 17 + 1) * box,
-      y: Math.floor(Math.random() * 15 + 3) * box
+      x: Math.floor(Math.random() * 16 + 1) * box,
+      y: Math.floor(Math.random() * 16 + 3) * box
     };
   } 
   else {
@@ -104,6 +123,12 @@ function update() {
   // desenha a cobra na tela
   drawSnake();
   // desenha a comida na tela
+  while(food.x > 600 && food.x < 0){
+    food.x = Math.floor(Math.random()) * box
+  }
+  while(food.y > 600 && food.y < 0){
+    food.y = Math.floor(Math.random()) * box 
+  }
   drawFood();
 };
 
@@ -120,7 +145,7 @@ function iniciarJogo() {
     update();
     drawSnake();
     drawFood();
-  }, 200);
+  }, 100);
   // atualiza a última direção escolhida pelo usuário
   document.addEventListener("keydown", changeDirection);
 }
@@ -137,6 +162,7 @@ function changeDirection(event) {
   }
   // atualiza a última direção escolhida pelo usuário
   lastDirection = direction;
+  speed *= 1.5/1.0;
 }
 
 // inicia o jogo ao clicar no botão "ImgSnake"
