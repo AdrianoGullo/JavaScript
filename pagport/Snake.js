559a8c3a3688b4bc653525a7910da79e96cf3eb6
AdrianoGullo <adrianogullo1@gmail.com>
2023-04-13 02:21:40 -0300

    Deletion

--- pagport/Snake.js
+++ pagport/Snake.js
@@ -1,16 +1,13 @@
-const GAME_WIDTH = 608;
-const GAME_HEIGHT = 608;
-const GAME_AREA_X = 0;
-const GAME_AREA_Y = 0;
+const GameWidht = 608;
+const GameWidth = 608;
+const GameAreaX = 0;
+const GameAreaY = 0;
 
 const ImgSnake = document.getElementById("start-game");
 const box1 = document.getElementById("boxSnake");
 const canvas = document.getElementById("snake");
 const ctx = canvas.getContext("2d");
 const box = 32;
-let speed = 150;
-let intervalId;
-let Score = 0;
 
 const CabeçaCobra = new Image();
 CabeçaCobra.src = 'pagport/Imagens/SnakeEyes/cabeçacobra.jpg';
@@ -41,10 +38,7 @@ let snake = [{
   y: 10 * box
 }];
 
-let direction;
-let lastDirection;
-
-function drawSnake() {
+function DesenhaCobra() {
   for(let i = 0; i < snake.length; i++){
     if(i === 0) {
       ctx.drawImage(CabeçaCobra, snake[i].x, snake[i].y, box, box);
@@ -54,11 +48,57 @@ function drawSnake() {
   }
 }
 
-function drawFood() {
+function DesenhaComida() {
   ctx.fillStyle = "red";
   ctx.fillRect(food.x, food.y, box, box);
 }
 
+let Score = 0;
+function DesenhaScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "20px Arial";
+  ctx.fillText("Score: " + Score, 20, 40);
+}
+
+let Poder = 0;
+function usarPoder(e){
+  switch(e){
+    case 1:
+      //cobra perde metade do tamanho
+      break;
+    case 2:
+      //caem 7 frutas no campo
+      break;
+    case 3:
+      //fica imune ao tocar o corpo por 10 segundos
+      break;
+    case 4:
+      //diminui a velocidade (usando SetInterval)
+      break;
+    case 5:
+      //matar todas as cobrinhas
+    default:
+      return 0;
+  }
+}
+
+let Direcao;
+let UltimaDirecao;
+function changeDirecao(event) {
+  if (event.keyCode === 37) {
+    Direcao = "left";
+  } else if (event.keyCode === 38) {
+    Direcao = "up";
+  } else if (event.keyCode === 39) {
+    Direcao = "right";
+  } else if (event.keyCode === 40) {
+    Direcao = "down";
+  }
+  if (event.keyCode === 32) {
+    usarPoder(Poder);
+  } 
+  UltimaDirecao = Direcao;
+}
 
 //Atualiza a posição da cobra com base na última direção escolhida pelo usuário
 function update() {
@@ -69,28 +109,28 @@ function update() {
     y: snakeY
   };
 
-  if (lastDirection === "right") {
+  if (UltimaDirecao === "right") {
     snakeX += box;
-    if (snakeX >= GAME_AREA_X + GAME_WIDTH) {
-      snakeX = GAME_AREA_X;
+    if (snakeX >= GameAreaX + GameWidht) {
+      snakeX = GameAreaX;
     }
   }
-  if (lastDirection === "left") {
+  if (UltimaDirecao === "left") {
     snakeX -= box;
-    if (snakeX < GAME_AREA_X) {
-      snakeX = GAME_AREA_X + GAME_WIDTH - box;
+    if (snakeX < GameAreaX) {
+      snakeX = GameAreaX + GameWidht - box;
     }
   }
-  if (lastDirection === "up") {
+  if (UltimaDirecao === "up") {
     snakeY -= box;
-    if (snakeY < GAME_AREA_Y) {
-      snakeY = GAME_AREA_Y + GAME_HEIGHT - box;
+    if (snakeY < GameAreaY) {
+      snakeY = GameAreaY + GameWidth - box;
     }
   }
-  if (lastDirection === "down") {
+  if (UltimaDirecao === "down") {
     snakeY += box;
-    if (snakeY >= GAME_AREA_Y + GAME_HEIGHT) {
-      snakeY = GAME_AREA_Y;
+    if (snakeY >= GameAreaY + GameWidth) {
+      snakeY = GameAreaY;
     }
   }
 
@@ -101,7 +141,7 @@ function update() {
       x: Math.floor(Math.random() * 16 + 1) * box,
       y: Math.floor(Math.random() * 16 + 3) * box
     };
-   Score += 10;
+    Score += 10;
   } 
   else {
     snake.pop();
@@ -112,41 +152,21 @@ function update() {
     snake.unshift(newHead);
   }
 
-  drawSnake();
-  drawFood();
+  DesenhaCobra();
+  DesenhaComida();
 };
-
-function drawScore() {
-  ctx.fillStyle = "white";
-  ctx.font = "20px Arial";
-  ctx.fillText("Score: " + Score, 20, 40);
-}
 
 function iniciarJogo() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSnake();
+  DesenhaCobra();
   setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     update();
-    drawSnake();
-    drawFood();
-    drawScore();
+    DesenhaCobra();
+    DesenhaComida();
+    DesenhaScore();
   }, 100);
-  document.addEventListener("keydown", changeDirection);
-}
-
-function changeDirection(event) {
-  if (event.keyCode === 37) {
-    direction = "left";
-  } else if (event.keyCode === 38) {
-    direction = "up";
-  } else if (event.keyCode === 39) {
-    direction = "right";
-  } else if (event.keyCode === 40) {
-    direction = "down";
-  }
-  lastDirection = direction;
-  speed *= 1.5/1.0;
+  document.addEventListener("keydown", changeDirecao);
 }
 
 ImgSnake.addEventListener("click", function() {
